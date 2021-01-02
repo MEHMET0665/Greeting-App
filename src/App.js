fa8bbc16f7809d7329b76e1434710a78f630b397
Memet <meavci@gmail.com>
2021-01-02 15:48:47 -0500

    convert function compomenet to the class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-function App() {
-  return (
-    <div>
-    <h1>Hello Guest</h1>
-    <label htmlFor="username">Enter you name:</label><br/>
-    <input id="username" type="text"/>
-    </div>
-  );
+import { ReactComponent } from "*.svg";
+
+import React from"react"
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+      <h1>Hello Guest</h1>
+      <label htmlFor="username">Enter you name:</label><br/>
+      <input id="username" type="text"/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
