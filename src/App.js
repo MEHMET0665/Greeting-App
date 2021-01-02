4a9feef6efd1f240a0ca58160a489417f980f309
Memet <meavci@gmail.com>
2021-01-02 16:04:12 -0500

    add constructor with state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { ReactComponent } from "*.svg";
+import React from 'react'
 
-import React from"react"
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      userName:'Guest'
+    }
+  }
+  render() {
     return (
       <div>
-      <h1>Hello Guest</h1>
-      <label htmlFor="username">Enter you name:</label><br/>
-      <input id="username" type="text"/>
+        <h1>Hello {this.state.userName}</h1>
+        <label htmlFor="user_name">Enter your name:</label> <br/>
+        <input id="user_name" type="text"/>
       </div>
     );
   }
-  
 }
 
 export default App;
